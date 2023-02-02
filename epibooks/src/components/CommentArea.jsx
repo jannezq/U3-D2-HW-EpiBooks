1c200f26350f799c9b59eabd6fb5ac0c304e4bb1
Jovellyn Quiapos <jovellynanne11@gmail.com>
2023-02-02 12:57:33 +0000

    addec commentArea by selection working in progress

---
+++ epibooks/src/components/CommentArea.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class CommentArea extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Testing</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CommentArea;
--- epibooks/src/components/SingleBook.jsx
+++ epibooks/src/components/SingleBook.jsx
@@ -1,36 +1,50 @@
-import { Component } from "react"
-import {  Card } from "react-bootstrap"
+import CommentArea from "./CommentArea";
+import { Component } from "react";
+import { Card, Row, Col } from "react-bootstrap";
 import "../../src/App.css";
-import "./componentCSS.css"
+import "./componentCSS.css";
 
-class SingleBook extends Component{
-
-    state = {
-        selectedBook: false
-    }
-render() {
-    return(
-        <Card className={ this.state.selectedBook ? "selected card-area" : "card-area" }   onClick={() => {
-            if(this.state.selectedBook === false){
-                 this.setState({ selectedBook: true})
-                //  console.log('Clicked!', this.props.title)
-            } else{
-                this.setState({ selectedBook: false})
-                // console.log("You unselected the book", this.props.title)
-            }
-
-        }}
-
-        >
-            <Card.Img variant="top" src={this.props.img} className="image-area"/>
-            <Card.Body className="d-flex flex-column justify-content-between align-items-center">
-                   <Card.Title>{this.props.title}</Card.Title>
-            </Card.Body>
-        </Card>
-)
+class SingleBook extends Component {
+  state = {
+    selectedBook: false,
+    commentsOnLoad: false,
+  };
+  render() {
+    return (
+      <>
+        <Row className="d-flex flex-column">
+          <Col>
+            <Card
+              className={
+                this.state.selectedBook ? "selected card-area" : "card-area"
+              }
+              onClick={() => {
+                if (this.state.selectedBook === false) {
+                  this.setState({ selectedBook: true });
+                  this.setState({ commentsOnLoad: true });
+                  //  console.log('Clicked!', this.props.title)
+                } else {
+                  this.setState({ selectedBook: false });
+                  this.setState({ commentsOnLoad: false });
+                  // console.log("You unselected the book", this.props.title)
+                }
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={this.props.img}
+                className="image-area"
+              />
+              <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+                <Card.Title>{this.props.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>{this.state.commentsOnLoad && <CommentArea />}</Col>
+        </Row>
+      </>
+    );
+  }
 }
 
-
-}
-
-export default SingleBook
+export default SingleBook;
